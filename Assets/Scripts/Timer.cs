415b2d8a450364290a1623123c2af11b7105b27d
MatiasChiprut <92793353+MatiasCHiprut@users.noreply.github.com>
2022-06-09 14:31:32 -0300

    Update 09/06

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public Text txtTime;
+    public bool timer=false;
+
+    public float MaxTime=0;
+    int limite=45;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer == false)
+        {
+            timer = true;
+            StartCoroutine(MaxTime());
+        }
+        if (MaxTime > limite)
+        {
+            SceneManager.LoadScene("You-Lost");
+        }
+    }
+
+    IEnumerator timer()
+    {
+
+    }
+}
