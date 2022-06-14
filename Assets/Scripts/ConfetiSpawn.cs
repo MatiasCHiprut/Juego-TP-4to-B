7eac9c18fb659dfff6aa2af690bf17524421ac6a
MatiasChiprut <92793353+MatiasCHiprut@users.noreply.github.com>
2022-06-14 15:53:12 -0300

    Update ConfetiSpawn.cs

--- Assets/Scripts/ConfetiSpawn.cs
+++ Assets/Scripts/ConfetiSpawn.cs
@@ -18,16 +18,17 @@ public class ConfetiSpawn : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            //int contador = 0;
-            //while (contador < cantmaxcubos)
-            //{
-            //   clone = Instantiate(cubePrefab, transform.position, Quaternion.identity);
-            //    contador++;
-            //}
-            //Destroy(clone, 3);
+            int contador = 0;
+            while (contador < cantmaxcubos)
+            {
+               clone = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+                contador++;
+                Destroy(clone, 3);
+            }
+           
 
-            clone = Instantiate(cubePrefab, transform.position, Quaternion.identity);
-            Destroy(clone, 3);
+            //clone = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+            //Destroy(clone, 3);
         }
 
     }
